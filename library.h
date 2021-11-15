d8a224d4fb44164bfc08b59d9e5ad528e50970c5
lhh1171 <2807652103@qq.com>
2021-11-15 21:45:04 +0800

    tt

---
+++ library.h
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define INITSIZE 1000
+typedef struct KEYVALUE{
+    char * key;
+    char * value;
+    struct KEYVALUE * next;
+}KeyValue;
+
+typedef struct HASHTABLE{
+    KeyValue ** kv;
+    int size;
+    int isInit;
+    int length;
+    int index;
+    int postions[1000];
+}HashTable;
+
+void removeKV(char * key);
+void put(char * key,char * value);
+char* _get(char * key);
+static int hash(char * key,int base);
+static void init();
+void foreach();
+static void extendCapcity();
+static KeyValue * findKey(char * key,int hashcode);
